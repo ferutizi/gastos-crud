f1a02fcac1be8b4382f78179bd0ebac93b18dca0
ferutizi <fer.utizi@gmail.com>
2024-05-22 16:54:27 -0300

    add: created expense entity

---
+++ src/expenses/expense.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity({ name: 'expenses' })
+export class Expense {
+
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'date', default: () => 'CURRENT_TIMESTAMP' })
+  date: Date
+
+  @Column()
+  detail: string
+
+  @Column()
+  section: string
+}
