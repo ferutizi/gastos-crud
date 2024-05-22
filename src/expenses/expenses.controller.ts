1c6f5ea9fcb1bfd66815cfafc1c245c3201a540e
ferutizi <fer.utizi@gmail.com>
2024-05-22 17:45:04 -0300

    add: getExpenses controller

--- src/expenses/expenses.controller.ts
+++ src/expenses/expenses.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateExpenseDto } from 'src/dto/createExpense.dto';
 import { ExpensesService } from './expenses.service';
+import { Expense } from './expense.entity';
 
 @Controller('expenses')
 export class ExpensesController {
@@ -8,7 +9,12 @@ export class ExpensesController {
   constructor(private expensesService: ExpensesService) { }
 
   @Post()
-  createExpense(@Body() newExpense: CreateExpenseDto) {
+  createExpense(@Body() newExpense: CreateExpenseDto): Promise<Expense> {
     return this.expensesService.createExpense(newExpense)
+  }
+
+  @Get()
+  getExpenses(): Promise<Expense[]> {
+    return this.expensesService.getExpenses()
   }
 }
