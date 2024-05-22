79494f6fe2d2b38177938b2387ccb3114617d95e
ferutizi <fer.utizi@gmail.com>
2024-05-22 17:10:42 -0300

    add: create expense service to createExpense

--- src/expenses/expenses.service.ts
+++ src/expenses/expenses.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Expense } from './expense.entity';
+import { Repository } from 'typeorm';
+import { CreateExpenseDto } from 'src/dto/createExpense.dto';
 
 @Injectable()
-export class ExpensesService { }
+export class ExpensesService {
+  constructor(@InjectRepository(Expense) private expenseRepository: Repository<Expense>) { }
+
+  createExpense(expense: CreateExpenseDto) {
+    const newExpense = this.expenseRepository.create(expense)
+    return this.expenseRepository.save(newExpense)
+  }
+}
